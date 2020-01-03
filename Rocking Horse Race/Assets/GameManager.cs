028325acbc4acd7a95dfc469a25cfbe729f0926c
kbDucky <knborror@gmail.com>
2020-01-02 20:45:38 -0500

    Cleaned up the UI and put finishing touches on menus and connections to menus

--- Rocking Horse Race/Assets/GameManager.cs
+++ Rocking Horse Race/Assets/GameManager.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
 
     public bool gameHasEnded = false;
     public GameObject endGameUI;
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
     public GameObject brownHorse;
     public GameObject finishCamera;
     public Text winningHorse;
+    public GameObject askInstructions;
+    private bool isActive = false;
 
     public void EndGame(string horse) {
         if (gameHasEnded == false) {
@@ -18,22 +20,38 @@ public class GameManager : MonoBehaviour
             Debug.Log("Game Over: " + horse + " won");
             winningHorse.text = horse;
             endGameUI.SetActive(true);
-            
+
             whiteHorse.GetComponent<Movement2>().enabled = false;
             brownHorse.GetComponent<Movement>().enabled = false;
             finishCamera.SetActive(true);
+            StartCoroutine(AskReset());
         }
 
     }
 
     private void Update() {
-        // reset the game with the "r" key
+        // reset the game with the "r" key and go to main menu with "m" key for debugging
         if (Input.GetKey("r")) {
             Restart();
         }
+        if (Input.GetKey("m")) {
+            SceneManager.LoadScene("MainMenu");
+        }
+        if (Input.GetKey("a") && Input.GetKey(KeyCode.LeftArrow) && isActive) {
+            Restart();
+        }
+        if (Input.GetKey("d") && Input.GetKey(KeyCode.RightArrow) && isActive) {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
+
     void Restart() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    IEnumerator AskReset() {
+        yield return new WaitForSeconds(3);
+        askInstructions.SetActive(true);
+        isActive = true;
+    }
 }
--- Rocking Horse Race/Assets/Scripts/MainMenuControl.cs
+++ Rocking Horse Race/Assets/Scripts/MainMenuControl.cs
@@ -5,10 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuControl : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject instructions;
+
     void Update()
     {
-        if (Input.GetKey("r")) {
-            SceneManager.LoadScene("Game");
+        if (Input.GetKey("a") && Input.GetKey(KeyCode.LeftArrow)) {
+            menu.SetActive(false);
+            instructions.SetActive(true);
+            StartCoroutine(Wait());
         }
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Game");
     }
 }
